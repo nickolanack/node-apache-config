fa4b399ff9b7cec978a2657b34af0b53b62fe77f
Nick <nickblackwell@gmail.com>
2015-04-19 22:56:33 -0700

    added methods to apache.js

--- apache.js
+++ apache.js
@@ -2,11 +2,38 @@
  * 
  */
 
-var apache=function(){
-
-	
-	
-	
+module.exports={
+		
+		
+		/**
+		 * @returns the absolute path to the document root, for a give hostname, or alias
+		 */
+		getDocumentRoot:function(hostName){
+			return '/var/www/';
+		},
+		
+		/**
+		 * 
+		 * @returns array of strings, matching host names (not aliases)
+		 */
+		listNames:function(){
+			
+			require('child_process').exec('httpd -t -D DUMP_VHOSTS', function (err, stdout, stderr) {
+		    	//console.log('stderr: ' + stderr);
+		    	if (err)throw err;
+			var vhosts=require('./httpd-vhost-parser.js').parse(stdout);
+			
+			return vhosts.map(function(){
+				
+				
+				
+			});
+			
+			
+		},
+		
+		
+		
 }
 
 
