93378be5c99db36513bcf878a338ef1e8c9da17d
Nick <nickblackwell@gmail.com>
2015-04-19 23:05:13 -0700

    added callbacks args to methods

--- apache.js
+++ apache.js
@@ -8,27 +8,27 @@ module.exports={
 		/**
 		 * @returns the absolute path to the document root, for a give hostname, or alias
 		 */
-		getDocumentRoot:function(hostName){
-			return '/var/www/';
+		getDocumentRoot:function(hostName, callback){
+			callback('/var/www/');
 		},
 		
 		/**
 		 * 
 		 * @returns array of strings, matching host names (not aliases)
 		 */
-		listNames:function(){
+		getHosts:function(callback){
 			
 			require('child_process').exec('httpd -t -D DUMP_VHOSTS', function (err, stdout, stderr) {
 		    	//console.log('stderr: ' + stderr);
 		    	if (err)throw err;
-			var vhosts=require('./httpd-vhost-parser.js').parse(stdout);
+				var vhosts=require('./httpd-vhost-parser.js').parse(stdout);
+				
+					callback(vhosts.map(function(){
+					
+				}));
 			
-			return vhosts.map(function(){
-				
-				
-				
+			
 			});
-			
 			
 		},
 		
