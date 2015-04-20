ab2683c71e3aab3436ab0e0bb9ad30222708e96a
Nick <nickblackwell@gmail.com>
2015-04-20 00:13:34 -0700

    can now get document root given a hostname or alias should also work for wildcard aliases

--- apache.js
+++ apache.js
@@ -2,38 +2,114 @@
  * 
  */
 
+
+
+function getDocumentRoot(hostName, callback){
+	
+	
+	getHostMeta(hostName, function(vhost){
+		
+		require('fs').readFile(vhost.conf[0], function (err, data) {
+  			if (err) throw err;
+  			var config=require('./httpd-conf-parser.js').parse(data.toString(),vhost.conf[1]);
+  			
+  			//console.log(JSON.stringify(config));
+  			callback(config.root);
+		});
+		
+	});
+
+}
+
+
+function getHostMeta(hostName, callback){
+	
+	
+	var name=hostName;
+	name=name.split(':');
+	name=name[0];
+	name=name.split('/'); //strip any url path info
+	name=name[0];
+	
+	getHostsMeta(function(list){
+		
+		if(list.length===0)throw new Error('no hosts');
+		
+		var vhost;
+		var names;
+		var vname;
+		for(var v=0;v<list.length;v++){
+			vhost=list[v];
+			
+			
+			
+			var names=[vhost.name];
+			
+			if(vhost.aliases){
+				names=names.concat(vhost.aliases.map(function(a){return a.name;}));
+			}
+			
+			for(var n=0;n<names.length;n++){
+				
+				vname=names[n];
+				
+				if(vname===name){
+					//console.log(vname+'='+name+JSON.stringify(vhost));
+					callback(vhost);
+					return;
+				}
+					
+				if(vname.substring(0,2)==='*.'){
+					var p=name.indexOf(vname.substring(2));
+					if(p!==-1){
+						//console.log(vname+' in '+name+JSON.stringify(vhost));
+						callback(vhost);
+						return;
+					}
+				}
+			}
+			
+		}
+		
+		
+	});
+	
+};
+
+function getHostsMeta(callback){
+	
+	require('child_process').exec('httpd -t -D DUMP_VHOSTS', function (err, stdout, stderr) {
+    	//console.log('stderr: ' + stderr);
+    	if (err)throw err;
+		var vhosts=require('./httpd-vhost-parser.js').parse(stdout);
+		
+			callback(vhosts);
+		
+			/*
+			callback(vhosts.map(function(vhost){
+				//console.log(JSON.stringify(vhost));
+				return vhost; //.name;
+			}));
+			 */
+	
+	});
+	
+}
+
 module.exports={
 		
 		
 		/**
 		 * @returns the absolute path to the document root, for a give hostname, or alias
 		 */
-		getDocumentRoot:function(hostName, callback){
-			callback('/var/www/');
-		},
+		getDocumentRoot:getDocumentRoot,
 		
 		/**
 		 * 
 		 * @returns array of strings, matching host names (not aliases)
 		 */
-		getHosts:function(callback){
-			
-			require('child_process').exec('httpd -t -D DUMP_VHOSTS', function (err, stdout, stderr) {
-		    	//console.log('stderr: ' + stderr);
-		    	if (err)throw err;
-				var vhosts=require('./httpd-vhost-parser.js').parse(stdout);
-				
-					callback(vhosts.map(function(){
-					
-				}));
-			
-			
-			});
-			
-		},
-		
-		
-		
-}
+		getHostsMeta:getHostsMeta
+
+};
 
 
