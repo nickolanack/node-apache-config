ed54eda5922550282f67a0fca517dcf0fd381631
Nick <nickblackwell@gmail.com>
2015-05-25 13:38:48 -0700

    add try catch for vhost conf parser

--- httpd-conf-parser.js
+++ httpd-conf-parser.js
@@ -6,12 +6,28 @@ function parse(conf, line){
 	
 	conf=conf.split("\n").slice(line).join("\n");
 	
+	var root=null;
+	var log=null;
+	var error=null;
 	
-
-	var root=conf.split('DocumentRoot')[1].split("\n")[0].replace(/^\s+/g, '').split(' ')[0].replace("\r","");
-	var log=conf.split('CustomLog')[1].split("\n")[0].replace(/^\s+/g, '').split(' ')[0].replace("\r","");
-	var error=conf.split('ErrorLog')[1].split("\n")[0].replace(/^\s+/g, '').split(' ')[0].replace("\r","");
-
+	try{
+		//it is posible for a host config to have no document ie: proxy-pass vhost
+		root=conf.split('DocumentRoot')[1].split("\n")[0].replace(/^\s+/g, '').split(' ')[0].replace("\r","");
+	}catch(e){
+		
+	}
+	
+	try{
+		log=conf.split('CustomLog')[1].split("\n")[0].replace(/^\s+/g, '').split(' ')[0].replace("\r","");
+	}catch(e){
+		
+	}
+	
+	try{
+		error=conf.split('ErrorLog')[1].split("\n")[0].replace(/^\s+/g, '').split(' ')[0].replace("\r","");
+	}catch(e){
+		
+	}
 	return {root:root, log:log, error:error};
 
 
